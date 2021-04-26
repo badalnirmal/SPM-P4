cc6a44edcd7418a79bfd85f06c1d11c595f4cf2b
Nirmal Badal <nirmal.badal@mavs.uta.edu>
2021-04-25 19:38:41 -0500

    Random song chosen from the database Passed song chosen and array of SongID to gameplay intent

--- app/src/main/java/edu/example/guessthesong/Game.java
+++ app/src/main/java/edu/example/guessthesong/Game.java
@@ -2,7 +2,12 @@ package edu.example.guessthesong;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import java.util.ArrayList;
 
 public class Game extends AppCompatActivity {
 
@@ -10,5 +15,22 @@ public class Game extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+
+        Intent intent = getIntent();
+        //Song ID that are already chosen and Song that is currently picked.
+        ArrayList <Integer> song_chosen = (ArrayList<Integer>) intent.getSerializableExtra("song_chosen");
+        String song = intent.getStringExtra("song");
+        System.out.println(song_chosen + song);
+
+
+
+        Button back_button =findViewById(R.id.back_button);
+        back_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Game.this,Homepage.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/edu/example/guessthesong/Homepage.java
+++ app/src/main/java/edu/example/guessthesong/Homepage.java
@@ -8,8 +8,17 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.StringTokenizer;
+
 public class Homepage extends AppCompatActivity {
 
+    final static ArrayList<Integer> song_chosen = new ArrayList<Integer>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,14 +26,59 @@ public class Homepage extends AppCompatActivity {
 
         Button play_button = findViewById(R.id.play_button);
         Button exit_button = findViewById(R.id.exit_button);
+        final String[] song = new String[1];
+
 
         play_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(Homepage.this,Game.class);//AC1 is login activity page(change AC6 to AC1)
-                //check for the activity name for login activity and replace AC1.class
-                startActivity(intent);
-                recreate();// this is for the reloading of AC1 page //recheck with TA
+                BufferedReader reader = null;
+                try {
+                    reader = new BufferedReader(
+                            new InputStreamReader(getAssets().open("PhraseBank.txt"), "UTF-8"));
+
+                    // do reading, usually loop until end of file reading
+                    String mLine;
+                    Random rand = new Random();
+                    int chosen_song = rand.nextInt(19)+1;
+
+                    //song_played.add(chosen_song);
+                    while ((mLine = reader.readLine()) != null) {
+                        //process line
+                        for(int i=0; i<song_chosen.size();i++)
+                        {
+                            if(song_chosen.get(i)==chosen_song) {
+                                while (song_chosen.get(i) == chosen_song)
+                                    chosen_song = rand.nextInt(19)+1;
+                            }
+                        }
+
+                        //System.out.println(mLine);
+                        StringTokenizer st = new StringTokenizer(mLine,"|");
+                        if(Integer.parseInt(st.nextToken())== chosen_song)
+                            song[0] = mLine;
+                    }
+                    song_chosen.add(chosen_song);
+                    //System.out.println(song_chosen);
+                } catch (IOException e) {
+                    Toast.makeText(Homepage.this,"File Can't be opened.\n Exiting",Toast.LENGTH_LONG).show();
+                    finishAffinity();
+                } finally {
+                    if (reader != null) {
+                        try {
+                            reader.close();
+                        } catch (IOException e) {
+                            //log the exception
+                        }
+                        Intent intent = new Intent(Homepage.this,Game.class);
+                        intent.putExtra("song_chosen", song_chosen);
+                        String song1=song[0];
+                        //System.out.println(song1);
+                        intent.putExtra("song",song1);
+                        startActivity(intent);
+                        recreate();
+                    }
+                }
             }
         });
 
@@ -32,7 +86,7 @@ public class Homepage extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Toast.makeText(Homepage.this,"Exiting",Toast.LENGTH_SHORT).show();
-                finish();
+                finishAffinity();
             }
         });
     }
